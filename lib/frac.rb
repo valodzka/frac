906036a2f82ed46aa2d4f926bb867b5a90927474
Pavel Valodzka <pavel@valodzka.name>
2010-05-25 19:45:06 +0300

    fix comparison

--- lib/frac.rb
+++ lib/frac.rb
@@ -5,6 +5,6 @@ module Math
   # Find rational approximation to given real number.
   def self.frac(float, prec)
     arr = find_fracs(float, prec)
-    arr[2] > arr[5] ? Rational(arr[3], arr[4]) : Rational(arr[0], arr[1])
+    arr[2].abs > arr[5].abs ? Rational(arr[3], arr[4]) : Rational(arr[0], arr[1])
   end
 end
